cdf919a7d763ca26d2c2245c269ea587632d6b53
Nicola Galeotti <nicola.galeotti9@gmail.com>
2020-01-04 20:31:53 +0100

    ricopiato

---
+++ CalcLibrary/Temp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLibrary
+{
+    class Temp
+    {
+        public static double MediaTemperature(double[] temperature)
+        {
+            double resp = 0;
+            if (temperature != null && temperature.Length > 0)
+            {
+                int quantita = temperature.Length;
+                double somma = 0;
+                for (int i = 0; i < quantita; i++)
+                {
+                    somma = somma + temperature[i];
+                }
+                resp = somma / quantita;
+            }
+            else
+                resp = double.NaN;
+            return resp;
+        }
+    }
+}
